d3651e6ae0984a48f0cc4b2d121c3110cd1eacdd
Elijah Monjardin <monjardinelijah120@gmail.com>
2023-02-11 18:10:04 +0800

    formatted post news feed

--- src/components/Home.js
+++ src/components/Home.js
@@ -169,8 +169,59 @@ function Home(){
             return(
                 <div className="container-fluid" style={{padding:0}}>
                     <NavigationBar></NavigationBar>
-                    Welcome back, {user.currentUser.firstName}
-                    Put some fucking data heere
+                    <div className="bg-secondary container-sm">
+                        <div className="col-md-6 m-auto">
+                        <div className="row d-flex justify-content-center">
+                            <div className="mb-5">
+                                <div className="card">
+                                <div className="card-body p-4">
+                                    <div className="d-flex flex-start w-100">
+                                    <img className="rounded-circle shadow-1-strong me-3"
+                                        src={user.currentUser.defaultProfile} alt="avatar" width="65"
+                                        height="65" />
+                                    <div className="w-100">
+                                        <h5>Whats in your mind, {user.currentUser.firstName}?</h5>
+                                    
+                                        <div className="form-outline">
+                                        <textarea className="form-control" id="textAreaExample" rows="2" style={{resize: "none"}}></textarea>
+                                        <label className="form-label" for="textAreaExample"></label>
+                                        </div>
+                                        <div className="d-flex justify-content-between">
+                                        <button type="button" className="btn btn-success">Add Image</button>
+                                        <button type="button" className="btn btn-danger">
+                                            Send <i className="fas fa-long-arrow-alt-right ms-1"></i>
+                                        </button>
+                                        </div>
+                                    </div>
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="bg-secondary container-sm">
+                        <div className="card col-md-6 m-auto">
+                            <div className="card-body">
+                            <h4><img className="me-2"style={{height:40, borderRadius: 150 / 2,overflow:"hidden"}}src="https://img.freepik.com/free-vector/cute-shiba-inu-dog-super-hero-cartoon-vector-icon-illustration-animal-holiday-icon-concept-isolated_138676-7106.jpg?w=740&t=st=1676104982~exp=1676105582~hmac=1cbd12db665671734c89e695848d36f227af35abe22414447b5ebb7c773b1aa3"></img> Skating</h4>
+                                
+                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                            <img src={user.currentUser.defaultProfile} className="card-img-bottom" alt="..."/>
+                            <p className="d-flex justify-content-end">
+                            <button type="button" className="btn btn-primary">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-chat mb-1 me-2" viewBox="0 0 16 16">
+                                <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"></path>
+                                </svg>
+                                Add comment
+                            </button>
+                            </p>
+                        </div>
+                    </div>
+                    
+
                 </div>
             )
         }
