1418c7c32543a209040430c3ce55978a7e4abae3
Elijah Monjardin <monjardinelijah120@gmail.com>
2023-02-15 11:09:45 +0800

    created search format

--- src/components/Search.js
+++ src/components/Search.js
@@ -45,13 +45,58 @@ function Search(){
             console.log("Redirect")
         }else{
             console.log(showUser)
-            console.log("Authenticated")
+            
+            return(
+                <div className="container bg-secondary">
+                    <div class="card mb-3 mx-auto" style={{maxWidth: 540}}>
+                    <div class="row g-0">
+                        <div class="col-md-4 mt-3 mb-3 text-center ps-3">
+                        <img src="https://images5.alphacoders.com/112/1123013.jpg" class="img-fluid img-thumbnail" alt="..."
+                            style={{maxWidth: 170, maxHeight: 170,minHeight: 170, minWidth: 170,objectFit:"cover",zIndex: 1}}
+                        />
+                        </div>
+                        <div class="col-md-8">
+                        <div class="card-body h-100 d-flex justify-content-between flex-column ms-2">
+                            <div>
+                                <h5 class="card-title">Elijah Monjardin</h5>
+                                <p class="card-text">monjardinelijah121@gmail.com</p>
+                                <div class="d-flex justify-content-center rounded-3 p-2 mb-2"
+                                style={{backgroundColor: "#efefef"}}>
+                                    
+                                        <div>
+                                            <p class="small text-muted mb-1">Articles</p>
+                                            <p class="mb-0">41</p>
+                                        </div>
+                                        <div class="px-3">
+                                            <p class="small text-muted mb-1">Followers</p>
+                                            <p class="mb-0">976</p>
+                                        </div>
+                                        <div>
+                                            <p class="small text-muted mb-1">Rating</p>
+                                            <p class="mb-0">8.5</p>
+                                        </div>
+
+                                    
+                                </div>
+                                <div className="my-auto ms-auto d-flex justify-content-end ">
+                                        <button className="btn btn-secondary btn-sm ">View Profile</button>
+                                    </div>
+                            </div>
+                            
+                            
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            )
         }
         
     }
 
     return(
         <div className="container-fluid">
+        <NavigationBar></NavigationBar>
             {isAuthenticated()}
         </div>
     )
