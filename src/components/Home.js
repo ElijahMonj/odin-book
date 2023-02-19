a592dcf65c42d6dbc468506ea21ec690368e6be1
Elijah Monjardin <monjardinelijah120@gmail.com>
2023-02-19 11:33:25 +0800

    Added post and comment section

--- src/components/Home.js
+++ src/components/Home.js
@@ -103,9 +103,6 @@ function Home(){
        
     }
     function getPosts(){
-        console.log("---getPost()---")
-       
-        let posts;
         
         let followingUsers=[];
         user.currentUser.following.forEach(function(uid) {
@@ -139,7 +136,7 @@ function Home(){
                 ) 
             })
         })
-        console.log(object)
+
         return object;
     }
     
@@ -211,7 +208,7 @@ function Home(){
         
     }
     function comment(e){
-        console.log(e)
+       
         
     }
     function homepage(){
@@ -290,7 +287,7 @@ function Home(){
                 <div className="container-fluid" style={{padding:0}}>
                     <NavigationBar></NavigationBar>
                     <div className="bg-secondary container-lg">
-                        <div className="col-md-6 m-auto">
+                        <div className="col-lg-6 m-auto">
                         <div className="row d-flex justify-content-center">
                             <div className="mb-5">
                                 <div className="card">
@@ -301,7 +298,6 @@ function Home(){
                                         height="65" />
                                     <div className="w-100">
                                         <h5>Whats in your mind, {user.currentUser.firstName}?
-                    
                                         </h5>
                                         
                                         <div className="form-outline">
@@ -351,10 +347,10 @@ function Home(){
                             
                         }
                         return (
-                            <div className="card col-md-6 m-auto mb-5" key={idx}>
+                            <div className="card col-lg-6 m-auto mb-5" key={idx}>
                             <div className="card-body">
                             <h4><img className="me-2" style={{height:40, width:40, objectFit:"cover",borderRadius: 150 / 2,overflow:"hidden"}} 
-                            src="https://img.freepik.com/free-vector/cute-cat-playing-box-cartoon-vector-icon-illustration-animal-nature-icon-concept-isolated-flat_138676-5000.jpg?w=740&t=st=1676189306~exp=1676189906~hmac=9489d110d5e9ace5391451d014e68c477cb02ac188e695e6cdc8023021ee0d36">
+                            src={p.profilePic}>
                             </img>{p.author}</h4>     
                                 <p className="card-text">{p.caption}</p>
                                 <p className="card-text"><small className="text-muted">{p.date}</small></p>
@@ -365,25 +361,111 @@ function Home(){
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-chat mb-1 me-2" viewBox="0 0 16 16">
                             <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
                             </svg>
-                            Like</button>
+                            </button>
                             <button type="button" className="btn btn-secondary" onClick={comment}  
                             data-bs-toggle="modal" data-bs-target={"#id"+p.id}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-chat mb-1 me-2" viewBox="0 0 16 16">
                                 <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"></path>
-                                </svg>Comment</button>
-                                <div className="modal fade" id={"id"+p.id} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div className="modal-dialog">
+                                </svg></button>
+                                <div className="modal fade " id={"id"+p.id} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div className="modal-lg modal-dialog modal-dialog-centered modal-fullscreen-lg-down">
                                     <div className="modal-content">
                                     <div className="modal-header">
-                                        <h1 className="modal-title fs-5" id="exampleModalLabel">{p.author}</h1>
+                                        <div className="modal-title fs-5 d-flex" id="exampleModalLabel">
+                                        <img className="me-2" style={{height:50, width:50, objectFit:"cover",borderRadius: 150 / 2,overflow:"hidden"}} 
+                                        src={p.profilePic}></img>
+                                        <div className="d-flex flex-column justify-content-center">
+                                            <div>
+                                            {p.author}
+                                            </div>
+                                            <small className="text-muted" style={{fontSize:15}}>
+                                            {p.date}
+                                            </small>
+                                        </div>
+                                        
+                                        </div>
+                                        
                                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                     </div>
                                     <div className="modal-body">
-                                        post
+                                    {p.caption}
+                                    <div className="card"style={{border:0}}>
+                                    
+                                    {withImage()}
+                                         <div className="btn-group mt-1 w-100" role="group" aria-label="Basic example">
+                            <button type="button" className="btn btn-secondary">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-chat mb-1" viewBox="0 0 16 16">
+                            <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                            </svg>
+                            </button>
+                            <button type="button" className="btn btn-secondary" onClick={comment}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-chat mb-1 " viewBox="0 0 16 16">
+                                <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"></path>
+                                </svg></button>
+                     
+
+                            <button type="button" className="btn btn-secondary">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-chat mb-1" viewBox="0 0 16 16">
+                            <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z"/>
+                            </svg></button>
+                            </div>
+                            <hr></hr>
+                            <div className="card-body">
+                                <h5 className="text-center">Recent Comments</h5>
+                    
+                                <div className="row">
+                                <div className="col">
+                                
+                                {p.comments.map(function(comment, idc){
+                                    if(p.comments===[]){
+                                        
+                                        return(
+                                            <div key={idc}>No comments yet.</div>
+                                        )
+                                    }else{
+
+                                    function findProfilePic(){
+                                        var pp = user.users.find(item => item._id === comment.author_id);
+                                        return pp.defaultProfile
+                                    }
+                                    function findName(){
+                                        var uu = user.users.find(item => item._id === comment.author_id);
+                                        return uu.firstName+" "+uu.lastName
+                                    }
+                                        
+                                    return(
+                                        <div className="d-flex flex-start my-2" key={idc}>
+                                        <img className="rounded-circle shadow-1-strong me-3"
+                                            src={findProfilePic()} alt="avatar" width="50"
+                                            height="50" />
+                                        <div className="flex-grow-1 flex-shrink-1">
+                                            <div>
+                                            <div className="d-flex  flex-column">
+                                                <p className="m-0">
+                                                {findName()} 
+                                                </p>
+                                                <span className="small text-muted">{comment.date} </span>
+                                                
+                                            </div>
+                                            <p className="mb-0">
+                                            {comment.content} 
+                                            </p>
+                                            </div>
+                                        </div>
+                                        </div>  
+                                    )
+                                    }
+                                   
+                                })}
+                                    
+                                </div>
+                                </div>
+                            </div>
                                     </div>
-                                    <div className="modal-footer">
-                                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                        <button type="button" className="btn btn-primary">Save changes</button>
+                                       
+                                    </div>
+                                    <div className="modal-footer ">
+                                   input comment
                                     </div>
                                     </div>
                                 </div>
@@ -392,7 +474,7 @@ function Home(){
                             <button type="button" className="btn btn-secondary">
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-chat mb-1 me-2" viewBox="0 0 16 16">
                             <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z"/>
-                            </svg>Share</button>
+                            </svg></button>
                             </div>
                         </div>
             
