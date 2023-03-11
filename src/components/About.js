424ec23a211d46b629abe33e54c843965724c672
Elijah Monjardin <monjardinelijah120@gmail.com>
2023-03-11 19:48:29 +0800

    Polishing about page

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,13 @@ import NavigationBar from "./Navbar";
 import aboutLogo from "./images/about.png"
 import missionLogo from "./images/mission.png"
 import visionLogo from "./images/vision.png"
+
+import BootstrapLogo from "./images/bootstrap.png"
+import ExpressLogo from "./images/express.png"
+import ReactLogo from "./images/react.png"
+import MongoDBLogo from "./images/mongodb.png"
+import PassportLogo from "./images/passport.png"
+import NodeLogo from "./images/node.png"
 function About(){
   const URL=process.env.REACT_APP_API_URL
     function showAbout(){
@@ -106,6 +113,48 @@ function About(){
                   </div>
               </div>
               
+            </div>
+            <hr/>
+            <div className="container madeWithLinks">
+              <div className="row text-center">
+                <div className="col">
+                  Made With:
+                </div>
+             </div>
+
+              <div className="row text-muted">
+                <div className="col col-md-4">
+                  <div className="row">
+                    <div className="col-md-6" >
+                    <a href="https://www.mongodb.com/"><img src={MongoDBLogo} className="img-fluid madeWith" />MongoDB</a>
+                    </div>
+                    <div className="col-md-6">
+                    <a href="https://expressjs.com/"><img src={ExpressLogo} className="img-fluid madeWith" />Express</a>
+                    </div> 
+                  </div>
+                </div>
+                <div className="col col-md-4">
+                  <div className="row mb-3">
+                    <div className="col-md-6">
+                    <a href="https://reactjs.org/"><img src={ReactLogo} className="img-fluid madeWith" />React</a>
+                    </div>
+                    <div className="col-md-6">
+                    <a href="https://nodejs.org/en/"><img src={NodeLogo} className="img-fluid madeWith"/>
+                    Node</a></div> 
+                  </div>
+                </div>
+                <div className="col col-md-4">
+                  <div className="row">
+                    <div className="col-md-6">
+                    <a href="https://getbootstrap.com/"><img src={BootstrapLogo} className="img-fluid madeWith" id="bsLogo"/>
+                    Bootstrap</a></div>
+                    <div className="col-md-6">
+                    <a href="https://www.passportjs.org/"><img src={PassportLogo} className="img-fluid madeWith" />
+                    Passport</a></div> 
+                  </div>
+                </div>
+              </div>
+
             </div>
             <footer className="bg-light text-center text-lg-start">
   
